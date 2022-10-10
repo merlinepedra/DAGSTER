4dea5d1b7e5e9a95e2daed8ceb9e7a15fe7f4041
prha <prha@elementl.com>
2022-10-10 09:59:44 -0700

    noop

--- python_modules/dagster/dagster/_core/storage/noop_compute_log_manager.py
+++ python_modules/dagster/dagster/_core/storage/noop_compute_log_manager.py
@@ -1,10 +1,19 @@
+from contextlib import contextmanager
+from typing import List, Optional
+
 import dagster._check as check
+from dagster._core.storage.captured_log_manager import (
+    CapturedLogData,
+    CapturedLogManager,
+    CapturedLogMetadata,
+    CapturedLogSubscription,
+)
 from dagster._serdes import ConfigurableClass, ConfigurableClassData
 
 from .compute_log_manager import MAX_BYTES_FILE_READ, ComputeLogFileData, ComputeLogManager
 
 
-class NoOpComputeLogManager(ComputeLogManager, ConfigurableClass):
+class NoOpComputeLogManager(CapturedLogManager, ComputeLogManager, ConfigurableClass):
     def __init__(self, inst_data=None):
         self._inst_data = check.opt_inst_param(inst_data, "inst_data", ConfigurableClassData)
 
@@ -47,4 +56,33 @@ class NoOpComputeLogManager(ComputeLogManager, ConfigurableClass):
         pass
 
     def on_unsubscribe(self, subscription):
+        pass
+
+    @contextmanager
+    def capture_logs(self, log_key: List[str]):
+        yield
+
+    def is_capture_complete(self, log_key: List[str]):
+        return True
+
+    def get_log_data(
+        self,
+        log_key: List[str],
+        cursor: Optional[str] = None,
+        max_bytes: Optional[int] = None,
+    ) -> CapturedLogData:
+        return CapturedLogData(log_key=log_key)
+
+    def get_contextual_log_metadata(self, log_key: List[str]) -> CapturedLogMetadata:
+        return CapturedLogMetadata()
+
+    def on_progress(self, log_key: List[str]):
+        pass
+
+    def subscribe(
+        self, log_key: List[str], cursor: Optional[str] = None
+    ) -> CapturedLogSubscription:
+        return CapturedLogSubscription(self, log_key, cursor)
+
+    def unsubscribe(self, subscription: CapturedLogSubscription):
         pass
