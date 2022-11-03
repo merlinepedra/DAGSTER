97f4a65a1ec029f712226fd90f5155881bb4012a
Chris DeCarolis <cdguitar817@gmail.com>
2022-11-03 14:42:22 -0700

    [jog] execution_tests/execution_plan_tests

--- python_modules/dagster/dagster_tests/execution_tests/execution_plan_tests/test_execution_plan.py
+++ python_modules/dagster/dagster_tests/execution_tests/execution_plan_tests/test_execution_plan.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dagster import DagsterInstance, Int, Output
+from dagster import job, Out, op, DagsterInstance, Int, Output
 from dagster import _check as check
 from dagster._core.definitions.pipeline_base import InMemoryPipeline
 from dagster._core.errors import (
@@ -17,36 +17,32 @@ from dagster._core.utils import make_new_run_id
 from dagster._legacy import (
     OutputDefinition,
     composite_solid,
-    execute_pipeline,
-    lambda_solid,
-    pipeline,
-    solid,
 )
 
 
 def define_diamond_pipeline():
-    @lambda_solid
+    @op
     def return_two():
         return 2
 
-    @solid
+    @op
     def add_three(num):
         return num + 3
 
-    @solid
+    @op
     def mult_three(num):
         return num * 3
 
-    @solid
+    @op
     def adder(left, right):
         return left + right
 
-    @pipeline
-    def diamond_pipeline():
+    @job
+    def diamond_job():
         two = return_two()
         adder(left=add_three(two), right=mult_three(two))
 
-    return diamond_pipeline
+    return diamond_job
 
 
 def test_topological_sort():
@@ -83,7 +79,9 @@ def test_active_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_1.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_1.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_1.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 2
@@ -96,13 +94,17 @@ def test_active_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_2.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_2.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_2.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_3.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_3.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_3.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
@@ -135,7 +137,9 @@ def test_failing_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_1.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_1.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_1.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 2
@@ -148,14 +152,18 @@ def test_failing_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_2.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_2.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_2.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
 
         # uh oh failure
         active_execution.mark_failed(step_3.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_3.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_3.key, "result")
+        )
 
         # cant progres to 4th step
         steps = active_execution.get_steps_to_execute()
@@ -200,7 +208,9 @@ def test_retries_active_execution():
         assert steps[0].key == "return_two"
 
         active_execution.mark_success(step_1.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_1.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_1.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 2
@@ -213,7 +223,9 @@ def test_retries_active_execution():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_2.key)
-        active_execution.mark_step_produced_output(StepOutputHandle(step_2.key, "result"))
+        active_execution.mark_step_produced_output(
+            StepOutputHandle(step_2.key, "result")
+        )
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -293,31 +305,31 @@ def test_retries_deferred_active_execution():
 
 
 def test_priorities():
-    @solid(tags={"priority": 5})
+    @op(tags={"priority": 5})
     def pri_5(_):
         pass
 
-    @solid(tags={"priority": 4})
+    @op(tags={"priority": 4})
     def pri_4(_):
         pass
 
-    @solid(tags={"priority": 3})
+    @op(tags={"priority": 3})
     def pri_3(_):
         pass
 
-    @solid(tags={"priority": 2})
+    @op(tags={"priority": 2})
     def pri_2(_):
         pass
 
-    @solid(tags={"priority": -1})
+    @op(tags={"priority": -1})
     def pri_neg_1(_):
         pass
 
-    @solid
+    @op
     def pri_none(_):
         pass
 
-    @pipeline
+    @job
     def priorities():
         pri_neg_1()
         pri_3()
@@ -391,28 +403,31 @@ def test_lost_steps():
             # failure assumed for start step - so rest should skip
             steps_to_abandon = active_execution.get_steps_to_abandon()
             while steps_to_abandon:
-                _ = [active_execution.mark_abandoned(step.key) for step in steps_to_abandon]
+                _ = [
+                    active_execution.mark_abandoned(step.key)
+                    for step in steps_to_abandon
+                ]
                 steps_to_abandon = active_execution.get_steps_to_abandon()
 
             assert active_execution.is_complete
 
 
 def test_fan_out_should_skip_step():
-    @solid(
-        output_defs=[
-            OutputDefinition(Int, "out_1", is_required=False),
-            OutputDefinition(Int, "out_2", is_required=False),
-            OutputDefinition(Int, "out_3", is_required=False),
-        ]
+    @op(
+        out={
+            "out_1": Out(Int, is_required=False),
+            "out_2": Out(Int, is_required=False),
+            "out_3": Out(Int, is_required=False),
+        }
     )
     def foo(_):
         yield Output(1, "out_1")
 
-    @solid
+    @op
     def bar(_, input_arg):
         return input_arg
 
-    @pipeline
+    @job
     def optional_outputs():
         foo_res = foo()
         # pylint: disable=no-member
@@ -421,7 +436,9 @@ def test_fan_out_should_skip_step():
         bar.alias("bar_3")(input_arg=foo_res.out_3)
 
     instance = DagsterInstance.ephemeral()
-    pipeline_run = PipelineRun(pipeline_name="optional_outputs", run_id=make_new_run_id())
+    pipeline_run = PipelineRun(
+        pipeline_name="optional_outputs", run_id=make_new_run_id()
+    )
     execute_plan(
         create_execution_plan(optional_outputs, step_keys_to_execute=["foo"]),
         InMemoryPipeline(optional_outputs),
@@ -447,40 +464,42 @@ def test_fan_out_should_skip_step():
 
 
 def test_fan_in_should_skip_step():
-    @lambda_solid
+    @op
     def one():
         return 1
 
-    @solid(output_defs=[OutputDefinition(is_required=False)])
+    @op(out=Out(is_required=False))
     def skip(_):
         return
         yield  # pylint: disable=unreachable
 
-    @solid
+    @op
     def fan_in(_context, items):
         return items
 
     @composite_solid(output_defs=[OutputDefinition(is_required=False)])
-    def composite_all_upstream_skip():
+    def graph_all_upstream_skip():
         return fan_in([skip(), skip()])
 
     @composite_solid(output_defs=[OutputDefinition(is_required=False)])
-    def composite_one_upstream_skip():
+    def graph_one_upstream_skip():
         return fan_in([one(), skip()])
 
-    @pipeline
+    @job
     def optional_outputs_composite():
-        composite_all_upstream_skip()
-        composite_one_upstream_skip()
+        graph_all_upstream_skip()
+        graph_one_upstream_skip()
 
     instance = DagsterInstance.ephemeral()
-    pipeline_run = PipelineRun(pipeline_name="optional_outputs_composite", run_id=make_new_run_id())
+    pipeline_run = PipelineRun(
+        pipeline_name="optional_outputs_composite", run_id=make_new_run_id()
+    )
     execute_plan(
         create_execution_plan(
             optional_outputs_composite,
             step_keys_to_execute=[
-                "composite_all_upstream_skip.skip",
-                "composite_all_upstream_skip.skip_2",
+                "graph_all_upstream_skip.skip",
+                "graph_all_upstream_skip.skip_2",
             ],
         ),
         InMemoryPipeline(optional_outputs_composite),
@@ -491,7 +510,7 @@ def test_fan_in_should_skip_step():
     assert should_skip_step(
         create_execution_plan(
             optional_outputs_composite,
-            step_keys_to_execute=["composite_all_upstream_skip.fan_in"],
+            step_keys_to_execute=["graph_all_upstream_skip.fan_in"],
         ),
         instance,
         pipeline_run.run_id,
@@ -501,8 +520,8 @@ def test_fan_in_should_skip_step():
         create_execution_plan(
             optional_outputs_composite,
             step_keys_to_execute=[
-                "composite_one_upstream_skip.one",
-                "composite_one_upstream_skip.skip",
+                "graph_one_upstream_skip.one",
+                "graph_one_upstream_skip.skip",
             ],
         ),
         InMemoryPipeline(optional_outputs_composite),
@@ -513,7 +532,7 @@ def test_fan_in_should_skip_step():
     assert not should_skip_step(
         create_execution_plan(
             optional_outputs_composite,
-            step_keys_to_execute=["composite_one_upstream_skip.fan_in"],
+            step_keys_to_execute=["graph_one_upstream_skip.fan_in"],
         ),
         instance,
         pipeline_run.run_id,
@@ -523,40 +542,42 @@ def test_fan_in_should_skip_step():
 def test_configured_input_should_skip_step():
     called = {}
 
-    @solid(output_defs=[OutputDefinition(is_required=False)])
+    @op(out=Out(is_required=False))
     def one(_):
         yield Output(1)
 
-    @solid
-    def solid_should_not_skip(_, input_one, input_two):  # pylint: disable=unused-argument
+    @op
+    def op_should_not_skip(_, input_one, input_two):  # pylint: disable=unused-argument
         called["yup"] = True
 
-    @pipeline
-    def my_pipeline():
-        solid_should_not_skip(one())
+    @job
+    def my_job():
+        op_should_not_skip(one())
 
-    run_config = {"solids": {"solid_should_not_skip": {"inputs": {"input_two": {"value": "2"}}}}}
-    execute_pipeline(my_pipeline, run_config=run_config)
+    run_config = {
+        "solids": {"op_should_not_skip": {"inputs": {"input_two": {"value": "2"}}}}
+    }
+    my_job.execute_in_process(run_config=run_config)
     assert called.get("yup")
 
     # ensure should_skip_step behave the same as execute_pipeline
     instance = DagsterInstance.ephemeral()
-    pipeline_run = PipelineRun(pipeline_name="my_pipeline", run_id=make_new_run_id())
+    pipeline_run = PipelineRun(pipeline_name="my_job", run_id=make_new_run_id())
     execute_plan(
         create_execution_plan(
-            my_pipeline,
+            my_job,
             step_keys_to_execute=["one"],
             run_config=run_config,
         ),
-        InMemoryPipeline(my_pipeline),
+        InMemoryPipeline(my_job),
         instance,
         pipeline_run,
         run_config=run_config,
     )
     assert not should_skip_step(
         create_execution_plan(
-            my_pipeline,
-            step_keys_to_execute=["solid_should_not_skip"],
+            my_job,
+            step_keys_to_execute=["op_should_not_skip"],
             run_config=run_config,
         ),
         instance,
